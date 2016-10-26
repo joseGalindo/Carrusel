0e6aed1f84fac54405e7e16c3551803ced812faf
Jose Galindo <jose.galindo@ciencias.unam.mx>
2016-10-26 11:17:30 -0500

    Initial Commit

---
+++ Carrusel/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Carrusel
+//
+//  Created by José Cruz Galindo Martínez on 26/10/16.
+//  Copyright © 2016 José Cruz Galindo Martínez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ Carrusel/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Carrusel
+//
+//  Created by José Cruz Galindo Martínez on 26/10/16.
+//  Copyright © 2016 José Cruz Galindo Martínez. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
