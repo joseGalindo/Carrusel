2c6b62452484d6c34ff2e9fd5a4f63c47ed91963
Jose Galindo <jose.galindo@ciencias.unam.mx>
2017-01-19 15:20:50 -0600

    Revision de biografias

---
+++ Carrusel/Classes/BuscadorViewController.h
@@ -0,0 +1,17 @@
+//
+//  BuscadorViewController.h
+//  Cuadros
+//
+//  Created by Gesfor on 19/01/17.
+//  Copyright © 2017 José Cruz Galindo Martínez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BuscadorViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UITextField *buscadorIn;
+@property (weak, nonatomic) IBOutlet UITableView *tableViewResult;
+
+
+@end
