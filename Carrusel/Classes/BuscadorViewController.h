e99c5516cb8f6086cfd10f69189fce98aad184bf
Jose Galindo <jose.galindo@ciencias.unam.mx>
2017-01-19 20:04:39 -0600

    Busqueda lista

--- Carrusel/Classes/BuscadorViewController.h
+++ Carrusel/Classes/BuscadorViewController.h
@@ -8,10 +8,13 @@
 
 #import <UIKit/UIKit.h>
 
-@interface BuscadorViewController : UIViewController
+@interface BuscadorViewController : UIViewController <UITextFieldDelegate, UITableViewDataSource>
 
 @property (weak, nonatomic) IBOutlet UITextField *buscadorIn;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *nombreApellidoSegmented;
 @property (weak, nonatomic) IBOutlet UITableView *tableViewResult;
 
+- (IBAction)atras:(id)sender;
+- (IBAction)buscar:(id)sender;
 
 @end
--- Carrusel/Classes/BuscadorViewController.m
+++ Carrusel/Classes/BuscadorViewController.m
@@ -7,31 +7,82 @@
 //
 
 #import "BuscadorViewController.h"
+#import "Carrusel-Swift.h"
+#import <MagicalRecord/MagicalRecord.h>
 
-@interface BuscadorViewController ()
-
+@interface BuscadorViewController () {
+  NSArray<Personaje*>* resultado;
+}
 @end
 
 @implementation BuscadorViewController
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
+  [super viewDidLoad];
+  
+  resultado = [NSArray array];
+  _buscadorIn.delegate = self;
+  _tableViewResult.dataSource = self;
+  NSLog(@"Buscador cargado");
 }
 
 - (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
 }
+
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+  [self.view endEditing:YES];
+}
+
+
+#pragma mark - UITableViewDataSource
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+  return resultado.count;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+  UITableViewCell* cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"Identificador"];
+  cell.textLabel.text = [resultado objectAtIndex:indexPath.row].nombreCompleto;
+  return cell;
+}
+
+#pragma mark - UITextFieldDelegate
+
+//-(BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string {
+//  NSLog(@"Should Change: %@", [textField.text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]]);
+//  
+//  return YES;
+//}
 
 /*
-#pragma mark - Navigation
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (IBAction)atras:(id)sender {
+  [self dismissViewControllerAnimated:YES completion:nil];
 }
-*/
+
+- (IBAction)buscar:(id)sender {
+  NSPredicate* predicado;
+  NSString* busqueda = _buscadorIn.text;// lowercaseString];
+  if (_nombreApellidoSegmented.selectedSegmentIndex == 0) {
+    predicado = [NSPredicate predicateWithFormat:@"nombre CONTAINS[cd] %@",busqueda];
+  } else {
+    predicado = [NSPredicate predicateWithFormat:@"apellidoPaterno CONTAINS[cd] %@",busqueda];
+  }
+  resultado = [Personaje MR_findAllWithPredicate:predicado];
+  NSLog(@"Numero de Resultados: %ld", resultado.count);
+  for (Personaje* person in resultado) {
+    NSLog(@"%@", person.nombreCompleto);
+  }
+  [_tableViewResult reloadData];
+}
 
 @end
